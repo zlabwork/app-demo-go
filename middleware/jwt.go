b19d4d47acc9ca60228134bfc31d619efb564f38
Joe <joe@xxtime.com>
2023-05-04 11:42:08 +0800

    error access

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"app"
 	"app/entity"
+	"app/msg"
 	"github.com/golang-jwt/jwt/v4"
 	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
@@ -17,6 +18,12 @@ func Jwt() echo.MiddlewareFunc {
 		},
 		SigningKey: []byte(os.Getenv("APP_KEY")),
 		ContextKey: app.ContextKey,
+		ErrorHandler: func(c echo.Context, err error) error {
+			return c.JSON(msg.OK, entity.RespData{
+				Code:    msg.ErrAccess,
+				Message: "error access",
+			})
+		},
 	}
 
 	return echojwt.WithConfig(config)
