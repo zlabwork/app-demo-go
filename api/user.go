fd7897f495e018f54850d63cc435ba9f0e833654
Joe <joe@xxtime.com>
2023-05-04 11:08:28 +0800

    parse user

--- api/user.go
+++ api/user.go
@@ -2,14 +2,13 @@ package api
 
 import (
 	"app"
-	"app/entity"
-	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func Me(c echo.Context) error {
-	user := c.Get(app.ContextKey).(*jwt.Token)
-	claims := user.Claims.(*entity.TokenClaims)
-	return c.String(http.StatusOK, "Welcome "+claims.UserId+"!")
+	user := app.GetUser(c)
+	return c.JSON(http.StatusOK, echo.Map{
+		"user_id": user.UserId,
+	})
 }
--- app.go
+++ app.go
@@ -1,5 +1,16 @@
 package app
 
+import (
+	"app/entity"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
 const (
 	ContextKey = "_token"
 )
+
+func GetUser(c echo.Context) *entity.TokenClaims {
+	user := c.Get(ContextKey).(*jwt.Token)
+	return user.Claims.(*entity.TokenClaims)
+}
