ffaee82e7a2f7fd2937beaa681101262f5c71985
Joe <joe@xxtime.com>
2023-05-04 15:09:30 +0800

    response

--- entity/resp.go
+++ entity/resp.go
@@ -4,5 +4,4 @@ type RespData struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
-	TraceId interface{} `json:"trace_id,omitempty"`
 }
